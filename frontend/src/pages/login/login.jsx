adea820242cd0001dd3610114776337db9aad441
SanjanaSingh29 <s.sanjanasingh2978@gmail.com>
2026-07-26 01:05:14 +0530

    Initial project structure

---
+++ frontend/src/pages/login/login.jsx
@@ -0,0 +1,12 @@
+function Login(){
+    return(
+        <div>
+            <h1>Internly AI</h1>
+            <h2>Welcome Back</h2>
+            <p>Sign in to continue your internship journey</p>
+        </div>
+
+    )
+}
+
+export default Login;
